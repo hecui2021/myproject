f7feb7bbfe3a8ded8607543f98d0debe9054c284
hecui2021 <83117082+hecui2021@users.noreply.github.com>
2023-04-29 00:05:00 +0800

    aaa

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-
+        // test
         SpringApplication.run(Application.class, args);
     }
 
